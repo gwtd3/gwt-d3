2598da215180bc6c4373f29f5ef0f0f591f74288
anthonime <schiochetanthoni@gmail.com>
2013-06-19 23:35:09 +0200

    added test selection propoerty
    Signed-off-by: anthonime <schiochetanthoni@gmail.com>

---
+++ gwt-d3-demo/src/main/java/com/github/gwtd3/demo/client/testcases/selection/TestSelectionProperty.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2013, Anthony Schiochet and Eric Citaire
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * The names Anthony Schiochet and Eric Citaire may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL MICHAEL BOSTOCK BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.github.gwtd3.demo.client.testcases.selection;
+
+import com.github.gwtd3.api.D3;
+import com.github.gwtd3.api.core.Datum;
+import com.github.gwtd3.api.core.Selection;
+import com.github.gwtd3.api.functions.DatumFunction;
+import com.github.gwtd3.api.svg.PathDataGenerator;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.ComplexPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TestSelectionProperty extends AbstractSelectionTest {
+
+	private static final String PROPERTY = "checked";
+
+	@Override
+	public void doTest(final ComplexPanel sandbox) {
+		testGetter();
+		testSetterConstantBoolean();
+		testSetterConstantDouble();
+		testSetterConstantString();
+		testSetterPathDataGenerator();
+		testSetterFunction();
+
+	}
+
+	protected void testSetterFunction() {
+		// works with single selection
+		Selection selection = givenASimpleSelection(new Label());
+		final String value = "1";
+		selection.attr(TestSelectionProperty.PROPERTY, new DatumFunction<String>() {
+			@Override
+			public String apply(final Element context, final Datum datum, final int index) {
+				return value;
+			}
+		});
+		assertEquals(value, getElementAttribute(0, TestSelectionProperty.PROPERTY));
+
+		// works with multiple selection
+		Selection selection2 = givenAMultipleSelection(new Label(), new Label(), new Label());
+		selection2.attr(TestSelectionProperty.PROPERTY, new DatumFunction<String>() {
+			@Override
+			public String apply(final Element context, final Datum datum, final int index) {
+				return value;
+			}
+		});
+		assertEquals(value, getElementAttribute(0, TestSelectionProperty.PROPERTY));
+		assertEquals(value, getElementAttribute(1, TestSelectionProperty.PROPERTY));
+		assertEquals(value, getElementAttribute(2, TestSelectionProperty.PROPERTY));
+
+	}
+
+	protected void testSetterConstantString() {
+		// works with single selection
+		Selection selection = givenASimpleSelection(new Label());
+		String value = "1";
+		selection.attr(TestSelectionProperty.PROPERTY, value);
+		assertEquals(value, getElementAttribute(0, TestSelectionProperty.PROPERTY));
+
+		// works with multiple selection
+		Selection selection2 = givenAMultipleSelection(new Label(), new Label(), new Label());
+		selection2.attr(TestSelectionProperty.PROPERTY, value);
+		assertEquals(value, getElementAttribute(0, TestSelectionProperty.PROPERTY));
+		assertEquals(value, getElementAttribute(1, TestSelectionProperty.PROPERTY));
+		assertEquals(value, getElementAttribute(2, TestSelectionProperty.PROPERTY));
+
+	}
+
+	protected void testSetterConstantDouble() {
+		// works with single selection
+		Selection selection = givenASimpleSelection(new Label());
+		double value = 3.56;
+		selection.attr(TestSelectionProperty.PROPERTY, value);
+		assertEquals("3.56", getElementAttribute(0, TestSelectionProperty.PROPERTY));
+
+		// works with multiple selection
+		Selection selection2 = givenAMultipleSelection(new Label(), new Label(), new Label());
+		selection2.attr(TestSelectionProperty.PROPERTY, value);
+		assertEquals("3.56", getElementAttribute(0, TestSelectionProperty.PROPERTY));
+		assertEquals("3.56", getElementAttribute(1, TestSelectionProperty.PROPERTY));
+		assertEquals("3.56", getElementAttribute(2, TestSelectionProperty.PROPERTY));
+	}
+
+	protected void testSetterConstantBoolean() {
+		boolean value = true;
+		String expectedValue = "true";
+		// works with single selection
+		Selection selection = givenASimpleSelection(new Label());
+		selection.property(TestSelectionProperty.PROPERTY, value);
+		assertEquals(expectedValue, getElementProperty(0, TestSelectionProperty.PROPERTY).asString());
+
+	}
+
+	protected void testSetterPathDataGenerator() {
+		PathDataGenerator generator = D3.svg().arc().innerRadius(1).outerRadius(2).startAngle(0).endAngle(2);
+		// works with single selection
+		Selection selection = givenASimpleSelection(new Label());
+		selection.attr(TestSelectionProperty.PROPERTY, generator);
+		String expectedValue = generator.generate(JavaScriptObject.createArray());
+		assertEquals(expectedValue, getElementAttribute(0, TestSelectionProperty.PROPERTY));
+
+		// works with multiple selection
+		Selection selection2 = givenAMultipleSelection(new Label(), new Label(), new Label());
+		selection2.attr(TestSelectionProperty.PROPERTY, generator);
+		assertEquals(expectedValue, getElementAttribute(0, TestSelectionProperty.PROPERTY));
+		assertEquals(expectedValue, getElementAttribute(1, TestSelectionProperty.PROPERTY));
+		assertEquals(expectedValue, getElementAttribute(2, TestSelectionProperty.PROPERTY));
+	}
+
+	protected void testGetter() {
+		// try a boolean
+		CheckBox cb = new CheckBox();
+		Selection sel = givenASimpleSelection(cb);
+		cb.getElement().setPropertyBoolean("checked", true);
+		assertEquals(true, sel.property("checked").asBoolean());
+
+		// with multiple selection, should return the first element
+		Selection selection2 = givenAMultipleSelection(createLabel(TestSelectionProperty.PROPERTY, "true"), createLabel(TestSelectionProperty.PROPERTY, "false"),
+				createLabel(TestSelectionProperty.PROPERTY, "false"));
+		assertEquals("true", selection2.property(TestSelectionProperty.PROPERTY).asString());
+	}
+
+	private Widget createLabel(final String attr, final String value) {
+		Label l = new Label();
+		l.getElement().setPropertyString(attr, value);
+		return l;
+	}
+}
