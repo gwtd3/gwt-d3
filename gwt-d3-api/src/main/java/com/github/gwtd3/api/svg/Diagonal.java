20fddbd0234ce7fc4b22d54d3d5fd3258a2692d4
Yifeng Evan Shi <evanshi09@gmail.com>
2013-07-18 11:53:43 -0700

    Added comments

--- gwt-d3-api/src/main/java/com/github/gwtd3/api/svg/Diagonal.java
+++ gwt-d3-api/src/main/java/com/github/gwtd3/api/svg/Diagonal.java
@@ -4,19 +4,35 @@ import com.github.gwtd3.api.Coords;
 import com.github.gwtd3.api.functions.DatumFunction;
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * Provides java access to d3js' SVG diagonals
+ * 
+ * @author <a href="mailto:evanshi09@gmail.com">Evan Shi</a>
+ *
+ */
 public class Diagonal extends PathDataGenerator{
     protected Diagonal() {
         super();
     }
     
-    public final native String apply(Coords x, Coords y) /*-{
-        return this.call(this, { source : x, target : y });
+    /**
+     * Applies the diagonal function to produce a string describing the path
+     * 
+     * @param source beginning of diagonal
+     * @param target end of diagonal
+     * @return resulting string descibing diagonal
+     */
+    public final native String apply(Coords source, Coords target) /*-{
+        return this.call(this, { source : source, target : target });
     }-*/;
     
-    public final native Diagonal projection(JavaScriptObject proj) /*-{
-        return this.projection(proj);
-    }-*/;
 
+    /**
+     * Registers a callback datum function for diagonal's projection
+     * 
+     * @param datumFunction the callback
+     * @return this diagonal object
+     */
     public final native Diagonal projection(DatumFunction<?> datumFunction) /*-{
         return this.projection(
                     function(d, i) {
