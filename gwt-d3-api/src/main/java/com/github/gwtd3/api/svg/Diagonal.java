af6eeab7c1b7a14e51c8ed1c266e51f1511298c9
Yifeng Evan Shi <evanshi09@gmail.com>
2013-07-22 12:06:00 -0700

    Added proper comments and more diagonal API
    Tidied up classes to better reflect d3js' API and added up proper
    documentation linking. TreeDemo also now more properly references the
    API.

--- gwt-d3-api/src/main/java/com/github/gwtd3/api/svg/Diagonal.java
+++ gwt-d3-api/src/main/java/com/github/gwtd3/api/svg/Diagonal.java
@@ -1,42 +1,126 @@
 package com.github.gwtd3.api.svg;
 
-import com.github.gwtd3.api.Coords;
 import com.github.gwtd3.api.functions.DatumFunction;
+import com.github.gwtd3.api.layout.Link;
+import com.github.gwtd3.api.layout.Node;
 import com.google.gwt.core.client.JavaScriptObject;
 
 /**
- * Provides java access to d3js' SVG diagonals
+ * Diagonal generator. Assumes the input data is an object with named attributes matching the accessors
+ * {@link Diagonal#source()} and {@link Diagonal#target()}. The returned function generates the path data for a cubic
+ * Bezier connecting the source and target points; the tangents are specified to produce smooth fan-in and fan-out when
+ * connecting nodes, as in a node-link diagram.
+ * 
+ * Diagonals default to Cartesian orientations, but can be used in radial and other orientations using
+ * {@link Diagonal#projection(DatumFunction)}.
  * 
  * @author <a href="mailto:evanshi09@gmail.com">Evan Shi</a>
- *
+ * 
  */
-public class Diagonal extends PathDataGenerator{
+public class Diagonal
+    extends PathDataGenerator {
     protected Diagonal() {
         super();
     }
-    
+
     /**
-     * Applies the diagonal function to produce a string describing the path
+     * Computes the path data string for the specified datum
      * 
-     * @param source beginning of diagonal
-     * @param target end of diagonal
-     * @return resulting string descibing diagonal
+     * @param the datum
+     * @return the path data string
      */
-    public final native String apply(Coords source, Coords target) /*-{
-        return this.call(this, { source : source, target : target });
+    public final native String apply(Link datum) /*-{
+		return this.call(this, datum);
     }-*/;
-    
+
+    /**
+     * Computes the path data string for the specified datum, passing in the specified index to the accessor functions
+     * 
+     * @param the datum
+     * @param index of the datum
+     * @return the path data string
+     */
+    public final native String apply(Link datum, int index) /*-{
+		this.call(this, datum, index);
+    }-*/;
+
+    /**
+     * @return the callback function registered with the diagonal generator
+     */
+    public final native DatumFunction<?> projection() /*-{
+		return this.projection();
+    }-*/;
 
     /**
      * Registers a callback datum function for diagonal's projection
      * 
-     * @param datumFunction the callback
+     * @param df the callback
      * @return this diagonal object
      */
-    public final native Diagonal projection(DatumFunction<?> datumFunction) /*-{
-        return this.projection(
-                    function(d, i) {
-                           return datumFunction.@com.github.gwtd3.api.functions.DatumFunction::apply(Lcom/google/gwt/dom/client/Element;Lcom/github/gwtd3/api/core/Value;I)(this,{datum:d},i);
-                       });
+    public final native Diagonal projection(DatumFunction<?> df) /*-{
+		return this
+				.projection(function(d, i) {
+					return df.@com.github.gwtd3.api.functions.DatumFunction::apply(Lcom/google/gwt/dom/client/Element;Lcom/github/gwtd3/api/core/Value;I)(this,{datum:d},i);
+				});
+    }-*/;
+
+    /**
+     * @return the current source accessor
+     */
+    public final native JavaScriptObject source() /*-{
+		return this.source();
+    }-*/;
+
+    /**
+     * @return the current target accessor
+     */
+    public final native JavaScriptObject target() /*-{
+		return this.target();
+    }-*/;
+
+    /**
+     * Sets the source accessor to be constant
+     * 
+     * @param a constant source
+     * @return the diagonal object
+     */
+    public final native Diagonal source(Node source) /*-{
+		return this.source(source);
+    }-*/;
+
+    /**
+     * Sets the target accessor to be constant
+     * 
+     * @param a constant target
+     * @return the diagonal object
+     */
+    public final native Diagonal target(Node target) /*-{
+		return this.target(target);
+    }-*/;
+
+    /**
+     * Sets the source accessor function per datum
+     * 
+     * @param the source accessor function
+     * @return the diagonal object
+     */
+    public final native Diagonal source(DatumFunction<?> df) /*-{
+		return this
+				.source(function(d, i) {
+					return df.@com.github.gwtd3.api.functions.DatumFunction::apply(Lcom/google/gwt/dom/client/Element;Lcom/github/gwtd3/api/core/Value;I)(this,{datum:d},i);
+				});
+    }-*/;
+
+    /**
+     * Sets the target accessor function per datum
+     * 
+     * @param the target accessor function
+     * @return the diagonal object
+     */
+    public final native Diagonal target(DatumFunction<?> df) /*-{
+		return this
+				.target(function(d, i) {
+					return df.@com.github.gwtd3.api.functions.DatumFunction::apply(Lcom/google/gwt/dom/client/Element;Lcom/github/gwtd3/api/core/Value;I)(this,{datum:d},i);
+				});
     }-*/;
 }
--- gwt-d3-api/src/main/java/com/github/gwtd3/api/svg/SVG.java
+++ gwt-d3-api/src/main/java/com/github/gwtd3/api/svg/SVG.java
@@ -39,76 +39,83 @@ import com.google.gwt.core.client.JavaScriptObject;
  * @author <a href="mailto:schiochetanthoni@gmail.com">Anthony Schiochet</a>
  * 
  */
-public class SVG extends JavaScriptObject {
+public class SVG
+    extends JavaScriptObject {
 
-	protected SVG() {
-	}
+    protected SVG() {
+    }
 
-	/**
-	 * Create a new arc with default accessor functions.
-	 * See {@link Arc} for details.
-	 * 
-	 * @return the arc
-	 */
-	public final native Arc arc()/*-{
+    /**
+     * Create a new arc with default accessor functions.
+     * See {@link Arc} for details.
+     * 
+     * @return the arc
+     */
+    public final native Arc arc()/*-{
 		return this.arc();
-	}-*/;
+    }-*/;
 
-	/**
-	 * Create a new default axis.
-	 * 
-	 * @return the axis
-	 */
-	public final native Axis axis()/*-{
+    /**
+     * Create a new default axis.
+     * 
+     * @return the axis
+     */
+    public final native Axis axis()/*-{
 		return this.axis();
-	}-*/;
+    }-*/;
 
-	/**
-	 * Create a new default line.
-	 * 
-	 * @return the line
-	 */
-	public final native Line line()/*-{
+    /**
+     * Create a new default line.
+     * 
+     * @return the line
+     */
+    public final native Line line()/*-{
 		return this.line();
-	}-*/;
+    }-*/;
 
-	/**
-	 * Create a new default area.
-	 * 
-	 * @return the area
-	 */
-	public final native Area area()/*-{
+    /**
+     * Create a new default area.
+     * 
+     * @return the area
+     */
+    public final native Area area()/*-{
 		return this.area();
-	}-*/;
+    }-*/;
 
-	/**
-	 * Create a new default brush.
-	 * 
-	 * @return the brush
-	 */
-	public final native Brush brush() /*-{
+    /**
+     * Create a new default brush.
+     * 
+     * @return the brush
+     */
+    public final native Brush brush() /*-{
 		return this.brush();
-	}-*/;
+    }-*/;
 
-	/**
-	 * Create a new default chord.
-	 * 
-	 * @return the chord
-	 */
-	public final native Chord chord() /*-{
+    /**
+     * Create a new default chord.
+     * 
+     * @return the chord
+     */
+    public final native Chord chord() /*-{
 		return this.chord();
-	}-*/;
-	
-	public final native Diagonal diagonal() /*-{
-	    return this.diagonal();
-	}-*/;
+    }-*/;
 
-	/**
-	 * Create a new default {@link Symbol}.
-	 * 
-	 * @return the symbol
-	 */
-	public final native Symbol symbol() /*-{
+    /**
+     * Create a new diagonal generator with the default accessor functions.
+     * See {@link Diagonal} for details.
+     * 
+     * @return the diagonal
+     */
+    public final native Diagonal diagonal() /*-{
+		return this.diagonal();
+    }-*/;
+
+    /**
+     * Create a new default {@link Symbol}.
+     * 
+     * @return the symbol
+     */
+    public final native Symbol symbol() /*-{
 		return this.symbol();
-	}-*/;
+    }-*/;
 }
