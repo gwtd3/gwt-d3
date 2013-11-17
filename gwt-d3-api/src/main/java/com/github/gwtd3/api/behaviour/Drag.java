83446e62deebc36d20baa00038ac8938ca3b12f4
anthoni <schiochetanthoni@gmail.com>
2013-11-17 17:48:16 +0100

    added doc on drag.origin

--- gwt-d3-api/src/main/java/com/github/gwtd3/api/behaviour/Drag.java
+++ gwt-d3-api/src/main/java/com/github/gwtd3/api/behaviour/Drag.java
@@ -137,21 +137,40 @@ public class Drag extends JavaScriptObject implements IsFunction {
 		return this.origin(o);
     }-*/;
 
+    /**
+     * Set the origin accessor to the specified function.
+     * <p>
+     * An origin accessor is used to determine the starting position (the
+     * “origin?) of the element being dragged; this allows the drag behavior to
+     * preserve the offset between the mouse position and the starting element
+     * position during drag. If the origin accessor is null, then the element
+     * position is set to the mouse position on drag; this can cause a
+     * noticeable jump on large elements. If an origin accessor is specified,
+     * the function is called on mousedown. The function is invoked in the same
+     * manner as other operator functions, being passed the current datum d and
+     * index i, with the this context as the clicked-on DOM element. To access
+     * the current event, use the global {@link D3#event()}. The origin accessor
+     * must return an object with x and y properties representing the starting
+     * coordinates of the element being dragged.
+     * <p>
+     * Frequently the origin accessor is specified as Object, or
+     * {@link D3#identity()}, which is equivalent to the identity function. This
+     * is suitable when the datum bound to the dragged element is already an
+     * object with x and y attributes representing its current position.
+     * <p>
+     * For example: <a
+     * href="http://bl.ocks.org/1557377">http://bl.ocks.org/1557377</a>
+     * 
+     * @param o
+     *            the origin accessor function, returning an object with x and y
+     *            attributes.
+     * @return the object Drag.
+     */
     public final native Drag origin(DatumFunction<Coords> o)/*-{
 		return this
 				.origin(function(d, i) {
 					return o.@com.github.gwtd3.api.functions.DatumFunction::apply(Lcom/google/gwt/dom/client/Element;Lcom/github/gwtd3/api/core/Value;I)(this,{datum:d},i);
 				});
     }-*/;
-
-    // public final native Drag origin(String s)/*-{
-    // return this.origin(function(d, i, s, u, l) {
-    // console.log('he');
-    // console.log(d);
-    // console.log('blu');
-    // //alert(d + ' ' + i + ' ' + s + ' ' + u + ' ' + l);
-    // return 100;
-    // });
-    // }-*/;
 
 }
