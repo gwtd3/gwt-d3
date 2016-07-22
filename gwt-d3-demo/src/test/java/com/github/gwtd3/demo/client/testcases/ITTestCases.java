1a71c581e3073b94c9ad8e327e31d3a3a085934b
Eric Citaire <ecitaire@mediametrie.fr>
2016-07-22 17:47:25 +0200

    Launch jetty and run Selenium tests in integration-test phase

--- gwt-d3-demo/src/test/java/com/github/gwtd3/demo/client/testcases/ITTestCases.java
+++ gwt-d3-demo/src/test/java/com/github/gwtd3/demo/client/testcases/ITTestCases.java
@@ -30,51 +30,25 @@ package com.github.gwtd3.demo.client.testcases;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-import org.openqa.selenium.interactions.Actions;
 
 import com.github.gwtd3.demo.client.pageobjects.DemoApplication;
-import com.github.gwtd3.demo.client.pageobjects.DemoDragMultiple.Circle;
+import com.github.gwtd3.demo.client.pageobjects.TestSuiteScreen;
 
-public class TestDragMultiples extends AbstractSeleniumTest {
+public class ITTestCases extends AbstractSeleniumTest {
 
 	@Test
-	public void testDragWorks() throws Exception {
-		// go to dragMultiples
-		Circle circle = DemoApplication.getInstance(driver).revealDemoDragMultiple()
-				// get the 5th circle
-				.getCircle(5);
-
-		// assert it's black
-		assertThat(circle.fillColor()).isNull();
-		// get its initial position
-		double x = circle.cx();
-		double y = circle.cy();
-		// hold mouse down on the circle
-		Actions a = new Actions(driver);
-		a.moveToElement(circle.getElement()).clickAndHold().build().perform();
-		// assert the circle is red
-		assertThat(circle.fillColor()).isEqualTo("red");
-		// hold mmouse down, mouse move
-		a.moveByOffset(10, 10).perform();
-		// assert the circle is green
-		assertThat(circle.fillColor()).isEqualTo("green");
-		// assert the current circle position is diff than initial position
-		Assertions.assertThat(circle.cx()).isEqualTo(x + 10);
-		Assertions.assertThat(circle.cy()).isEqualTo(y + 10);
-		x = circle.cx();
-		y = circle.cy();
-		// release mouse
-		a.release().perform();
-		// assert the circle is black
-		assertThat(circle.fillColor()).isNull();
-		// assert the current position is equals to previous
-		Assertions.assertThat(circle.cx()).isEqualTo(x);
-		Assertions.assertThat(circle.cy()).isEqualTo(y);
-		// move the mouse
-		a.moveByOffset(10, 10).perform();
-		// assert the positoin did not changed
-		Assertions.assertThat(circle.cx()).isEqualTo(x);
-		Assertions.assertThat(circle.cy()).isEqualTo(y);
-
+	public void basic() throws Exception {
+		DemoApplication application = DemoApplication.getInstance(driver);
+		Assertions.assertThat(driver.getTitle()).isEqualTo("GWT-D3 Demo");
 	}
+
+	@Test
+	public void testCasesPass() {
+		DemoApplication application = DemoApplication.getInstance(driver);
+
+		TestSuiteScreen testSuite = application.testCaseButton().click();
+		testSuite.clickRun().waitTestsAreAllDone();
+		Assertions.assertThat(testSuite.unitTestsAreSuccess()).isTrue();
+	}
+
 }
